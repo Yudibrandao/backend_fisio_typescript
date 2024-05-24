2c0e4fa090f8c3272e5e12f066a0b26b8e23c829
´Yudith´ <yudi@geeksacademy.es>
2024-05-24 12:09:37 +0200

    Autorizacion

--- src/Entities/Users/Router.ts
+++ src/Entities/Users/Router.ts
@@ -1,5 +1,6 @@
-import  express  from "express";
-import { login, register } from "./Controller";
+import  express, { request }  from "express";
+import { login, modify, register } from "./Controller";
+import { authorization } from "../../Middleware/Authorization";
 
 
 const router = express.Router()
@@ -21,8 +22,23 @@ router.post('/login', async (request, response)=> {
      response.status(201).json(await login(request.body))
     
    } catch (error) {
+
+    
     
    }
+})
+
+
+router.put('/modify', authorization, async (request, response)=> {
+  try {
+
+    response.status(201).json(await modify (request.body))
+
+  } catch (error) {
+    
+    
+  }
+
 })
 
 
---
+++ src/Middleware/Authorization.ts
@@ -0,0 +1,34 @@
+import Confidence from "../config_env";
+import jwt from "jsonwebtoken";
+
+
+const authorization = (request, response, next) => {
+    try {
+        const token = request.header("Authorization")
+
+        if (!token) {
+            return response.status(401).json({ succes: false, message: "NO AUTORIZADO" })
+        }
+
+        const tokenParts = token.split(" ")
+
+        if (tokenParts.length !== 2 || tokenParts[0] !== "Bearer") {
+            return response.status(401).json({ succes: false, message: "Formato de Token invalido" })
+        }
+
+        const validateToken = tokenParts[1]
+        request.user = jwt.verify(validateToken, Confidence.Secret_db)
+        next()
+
+    } catch (error) {
+
+        if (error instanceof jwt.JsonWebTokenError) {
+            return response.status(403).json({ error: "Token inválido" });
+        }
+        next(error);
+
+    }
+
+}
+
+export { authorization }
